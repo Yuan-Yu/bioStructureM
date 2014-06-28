47ad2fab0a6f46cab43aaecaaca875a7d54142a5
Yuan Yu <tearsgundam@gmail.com>
2014-06-28 20:44:22 +0800

    can make NMR pdb

--- cafrompdb.m
+++ cafrompdb.m
@@ -1,46 +1,57 @@
-function [ca,resnum] =cafrompdb(s)
-
-%function [c,bval] =CAfrompdb(s)
-%function pdb = pdbparse(s)
-% CAFROMPDB(S), where S is a string, refering to the file to be opened.
-i=0;
+function [ca] =cafrompdb(s,pdbType)
+if ~exist('pdbType', 'var')
+    pdbType='X-RAY';
+end
 j=0;
-DNA=1;
-countlastchain=0;
-
 lastresno=-1000;
-resnum=0;
+numOfMode=0;
+mode={};
 ca(1).resname =[];
 ca(1).atmname =[];
 ca(1).resno =[];
 ca(1).coord =[];
 ca(1).bval =[];
 ca(1).subunit =[];
-               fid=fopen(s);
-               line = fgetl(fid);
+fid=fopen(s);
+line = fgetl(fid);
 % revised by Lee 062504 - original:      while isstr(line)  % isstr wont be supported in the later version
+% altloc double(sscanf(line(17:17),'%c'))
 while ischar(line)
     if(length(line)>=66)
-        if (isequal(sscanf(line(1:6),'%c'),'ATOM  ') | isequal(sscanf(line(1:6),'%c'),'HETATM'))
-            if  ~(double(sscanf(line(17:17),'%c'))>65&&sscanf(line(23:26),'%i')==lastresno)
+        if isequal(sscanf(line(1:6),'%c'),'ATOM  ') || isequal(sscanf(line(1:6),'%c'),'HETATM')
+            if  sscanf(line(23:26),'%i')==lastresno && double(sscanf(line(17:17),'%c'))==currentAltloc || sscanf(line(23:26),'%i')~=lastresno
                 j=j+1;
+                ca(j).record  =  sscanf(line(1:6),'%c');
+                ca(j).atomno  =  sscanf(line(7:12),'%i');
                 ca(j).atmname =  sscanf(line(12:16),'%c');                
                 ca(j).resname =  sscanf(line(18:20),'%c');
                 ca(j).resno =  sscanf(line(23:26),'%i');
                 ca(j).coord =  sscanf(line(31:54),'%f %f %f');
                 ca(j).bval =  sscanf(line(61:66),'%f');
                 ca(j).subunit =  sscanf(line(22),'%c');
-                DNA=0;         
+                ca(j).occupancy=sscanf(line(55:60),'%f');
                 lastresno=sscanf(line(23:26),'%i');
+                currentAltloc=double(sscanf(line(17:17),'%c'));
             end
             
-%%%%%% Stop when 'END' is encountered and stop when the first model in NMR file was read %%%%%%%%%%%%
-        elseif(isequal(sscanf(line(1:6),'%c'),'END   ')|isequal(sscanf(line(1:6),'%c'),'ENDMDL'))
+        %%%%%% Stop when 'END' is encountered and stop when the first model in NMR file was read%%%%%%%%%%%%
+        elseif(isequal(sscanf(line(1:6),'%c'),'END   ')||(isequal(sscanf(line(1:6),'%c'),'ENDMDL')&&~isequal(pdbType,'NMR')))
                 break;
-        %%%%%% Stop when 'END' is encountered and stop when the first model in NMR file was read %%%%%%%%%%%%        
+        %%%%%% read all mode of NMR %%%%%%%%%%%%           
+        elseif(isequal(sscanf(line(1:6),'%c'),'ENDMDL'))
+            j=0;
+            lastresno=-1000;
+            numOfMode=numOfMode+1;
+            mode{numOfMode}=ca;   
         end
     end
         line = fgetl(fid);
+end
+
+if isequal(pdbType,'NMR')&& ~isempty(mode)
+    ca=mode;
+elseif isequal(pdbType,'NMR')
+    display('It seems like this pdb is not NMR structure!')
 end
 fclose(fid);
     
--- creatPDBbyCA.m
+++ creatPDBbyCA.m
@@ -1,10 +1,21 @@
 function creatPDBbyCA( ca,name )
 %CREATPDBBYCA Summary of this function goes here
 %   Detailed explanation goes here
- ffff=fopen(name,'w');
-   for i=1:length(ca)
-    fprintf(ffff,'ATOM       %4s %3s %s%4i    %8.3f%8.3f%8.3f      %6.2f\n',ca(i).atmname,ca(i).resname,ca(i).subunit,ca(i).resno,ca(i).coord(1),ca(i).coord(2),ca(i).coord(3),ca(i).bval);
-    end
-fclose(ffff);
+    fid=fopen(name,'w');
+   if ~iscell(ca)
+        for i=1:length(ca)
+        fprintf(fid,'ATOM       %4s %3s %s%4i    %8.3f%8.3f%8.3f      %6.2f\n',ca(i).atmname,ca(i).resname,ca(i).subunit,ca(i).resno,ca(i).coord(1),ca(i).coord(2),ca(i).coord(3),ca(i).bval);
+        end 
+   else
+       for i=1:length(ca)
+           fprintf(fid,'MODEL       %2i\n',i);
+           model=ca{i};
+           for j=1:length(model)
+               fprintf(fid,'%6s     %4s %3s %s%4i    %8.3f%8.3f%8.3f      %6.2f\n',model(j).record,model(j).atmname,model(j).resname,model(j).subunit,model(j).resno,model(j).coord(1),model(j).coord(2),model(j).coord(3),model(j).bval);
+           end
+           fprintf(fid,'ENDMDL');
+       end
+   end
+fclose(fid);
 end
 
