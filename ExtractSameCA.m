4f5d0e8eb3a9f5cca70a8867b5921ea0ebce90ee
Yuan Yu <tearsgundam@gmail.com>
2014-06-29 23:47:44 +0800

    change ExtractSameCA can extract all atom.

--- ExtractSameCA.m
+++ ExtractSameCA.m
@@ -3,23 +3,21 @@ function [newCA1,newCA2]=ExtractSameCA(ca1,ca2)
 %  Ca1,Ca2 is the object get from cafrompdb.m 
 %return newCA1 and newCA2 is the object get from cafrompdb. But only contain the same Ca atom.
     load('dict.mat');
+    tempca1=getAtomByAtomName(ca1,'CA');
+    tempca2=getAtomByAtomName(ca2,'CA');
 %% 
-    for i=1:length(ca1)
-        resName1(i,1)=dict.get(ca1(i).resname);
+    for i=1:length(tempca1)
+        resName1(i,1)=dict.get(tempca1(i).resname);
     end
-    for i=1:length(ca2)
-        resName2(i,1)=dict.get(ca2(i).resname);
+    for i=1:length(tempca2)
+        resName2(i,1)=dict.get(tempca2(i).resname);
     end
 %% 
     [StrX, StrY, Index_Seq1_Seq2, Align_Num TraceBack] = PA (resName1, resName2);
-    index_new=1;
-    for i=1:length(Index_Seq1_Seq2)
-        index1=Index_Seq1_Seq2(1,i);
-        index2=Index_Seq1_Seq2(2,i);
-        if index1 ~=0
-            newCA1(index_new)=ca1(index1);
-            newCA2(index_new)=ca2(index2);
-             index_new= index_new+1;
-        end
-    end
+    [row col]=find(Index_Seq1_Seq2);
+    %get the fist non-zero column index
+    sameca1=tempca1(Index_Seq1_Seq2(1,col(1):end));
+    sameca2=tempca2(Index_Seq1_Seq2(1,col(1):end));
+    newCA1=getAtomByResno(ca1,[sameca1.resno]);
+    newCA2=getAtomByResno(ca2,[sameca2.resno]);
         
