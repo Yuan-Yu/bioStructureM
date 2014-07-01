408975a3a4627fa7ac151846a14fe91df8f3092a
Yuan Yu <tearsgundam@gmail.com>
2014-07-01 14:59:14 +0800

    chagne Coordfromca to getCoordfromca

--- subtraction2CA.m
+++ subtraction2CA.m
@@ -5,8 +5,8 @@ function [ dR ] = subtraction2CA( ca1,ca2 )
 %return dR is the delta R(N*3) is like COORD format. 
    [ca1,ca2]=ExtractSameCA(ca1,ca2);
    [ca1]=superimpose(ca1,ca2);
-   coord1=Coordfromca(ca1);
-   coord2=Coordfromca(ca2);
+   coord1=getCoordfromca(ca1);
+   coord2=getCoordfromca(ca2);
    dR=coord1-coord2;
 end
 
