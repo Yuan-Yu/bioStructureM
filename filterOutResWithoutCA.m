c53e3f400cd2a83c3de92f696472291b526fb451
Yuan Yu <tearsgundam@gmail.com>
2014-07-05 22:06:10 +0800

    filterOutResWithoutCA:fix the bug that make mistake for muti-chain pdb.

--- filterOutResWithoutCA.m
+++ filterOutResWithoutCA.m
@@ -5,6 +5,9 @@ function [ca]=filterOutResWithoutCA(ca)
 % return:
 %    ca
 %%%%% need getAtomByAtomName,getAtomByResno%%%%%%%%
-temp=getAtomByAtomName(ca,'CA');
-resno=[temp.resno];
-ca=getAtomByResno(ca,resno);
+[resno_cell,chainID]=getResnoFromCA(ca);
+for i=1:length(chainID)
+    temp=getAtomByAtomName(getAtomByResno(ca,resno_cell(i),chainID(i)),'CA');
+    resno_cell{i}=[temp.resno];
+end
+ca=getAtomByResno(ca,resno_cell,chainID);
