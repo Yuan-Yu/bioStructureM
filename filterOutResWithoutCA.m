baa49adef190847b49221918fe3046ee28619d1f
YuanYu <tearsgundam@gmail.com>
2015-01-26 01:58:22 +0800

    modify the command

--- filterOutResWithoutCA.m
+++ filterOutResWithoutCA.m
@@ -7,7 +7,7 @@ function [ca]=filterOutResWithoutCA(ca)
 %%%%% need getAtomByAtomName,getAtomByResno%%%%%%%%
 [resno_cell,chainID]=getResnoFromCA(ca);
 for i=1:length(chainID)
-    temp=getAtomByAtomName(getAtomByResno(ca,resno_cell(i),chainID(i)),'CA');
+    temp=getAtomByAtomName(getAtomByResno(ca,resno_cell(i),0,chainID(i)),'CA');
     resno_cell{i}={temp.resno};
 end
-ca=getAtomByResno(ca,resno_cell,chainID);
+ca=getAtomByResno(ca,resno_cell,0,chainID);
--- superimpose.m
+++ superimpose.m
@@ -1,10 +1,12 @@
-function [ca1,eRMSD]=superimpose(ca1,ca2)
+function [ca1,eRMSD,R,T]=superimpose(ca1,ca2)
 %%%%%%%%  need rmsdfit.m,getCoordfromca %%%%%%%%%
 %the  ca1 and ca2 are the object gotten from cafrompdb 
 %the numbers of residus of ca1 and ca2 must be same
 % superimpose ca1 to ca2 
 %return ca1 is be superimposed
 %		eRMSD is the root-mean-square deviation of the two ca.
+%       R: rotation matrix
+%       T: translation matrix
 %%%%%%%%  need rmsdfit.m,getCoordfromca %%%%%%%%%
     COORD1=getCoordfromca(ca1);
     COORD2=getCoordfromca(ca2);
