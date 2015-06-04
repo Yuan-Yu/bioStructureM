2f85badfcf4e4babeba4848f5f9e09d8cf5105dc
Yuan Yu <tearsgundam@gmail.com>
2015-06-04 18:16:05 +0800

    new file: 		getGeometrycenter > ...

---
+++ getGeometrycenter.m
@@ -0,0 +1,10 @@
+function [geometrycenter]=getGeometrycenter(pdbStructure)
+%%%%%%%%%%%% need getCoord %%%%%%%%%%%%%%
+% input:
+%   pdbStructure: the struture array is getten from readPDB
+% reture:
+%   geometrycenter: the geometrycenter of the pdb
+%%%%%%%%%%%% need getCoord %%%%%%%%%%%%%%
+
+    geometrycenter=mean(getCoord(pdbStructure));
+end
