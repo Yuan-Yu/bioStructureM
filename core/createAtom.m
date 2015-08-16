8418108c613918aa52b17922147c076a51d51145
Yuan Yu <tearsgundam@gmail.com>
2015-08-17 02:20:30 +0800

    modified: 		atomfrompdb > add new attribute "internal residue number" 		to pdb structure array. 		readPDB > add new input to control whether assign mass. 		superimpose > change description 	new : 		createAtom > create one Atom.

---
+++ core/createAtom.m
@@ -0,0 +1,61 @@
+function [atomStructure] = createAtom(resname,atmname,coord,record,subunit,bval,atomno,resno)
+%%%%%%%%%%%%%%% need %%%%%%%%%%%%%%%
+% input:
+%   record       (chars)
+%   resname      (chars)
+%   coord        (3*1 double)
+%   subunit      (chars)
+%   bval         (double)
+%   atomno       (double)
+%   resno        (chars)
+%   elementSmbol (chars)
+%%%%%%%%%%%%%%% need %%%%%%%%%%%%%%%
+
+if ~exist('resname','var')
+    e = MException('core:noresname','resname is not defined');
+    throw(e);
+end
+if ~exist('atmname','var')
+    e = MException('core:noatmname','atmname is not defined');
+    throw(e);
+end
+if ~exist('coord','var')
+    coord = [0; 0 ;0];
+end
+if ~exist('record','var')
+    record = 'ATOM';
+end
+if ~exist('subunit','var')
+    subunit = 'A';
+end
+if ~exist('bval','var')
+    bval = 0;
+end
+if ~exist('atomno','var')
+    atomno = 1;
+end
+if ~exist('resno','var')
+    resno = '1';
+elseif class(resno) == 'double'
+    resno = num2str(resno);
+end
+
+
+
+atomStructure.resname = resname;
+atomStructure.record = record;
+atomStructure.atomno = atomno;
+atomStructure.atmname = atmname;
+atomStructure.resno = resno;
+atomStructure.iCode = '';
+atomStructure.coord = coord;
+atomStructure.bval = bval;
+atomStructure.subunit = subunit;
+atomStructure.occupancy = 0;
+atomStructure.alternate = ' ';
+atomStructure.charge = '';
+atomStructure.segment = '';
+atomStructure = setElementSymbol(atomStructure);
+atomStructure = setMass(atomStructure);
+
+
