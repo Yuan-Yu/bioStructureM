9adf2cfbfc47abe11b6e0325e29b23a7e46a76fc
Yuan Yu <tearsgundam@gmail.com>
2015-09-09 03:16:34 +0800

    extractSameCA > remove the part for genarating the tmpResno 	oneChainExtractSameCA > take care not only the residue level but 		also atom level.

--- core/oneChainExtractSameCA.m
+++ core/oneChainExtractSameCA.m
@@ -1,10 +1,15 @@
 function [newCA1,newCA2,StrX,StrY]=oneChainExtractSameCA(ca1,ca2)
-%######## need dict.mat,PA.m,getAtomByAtomName#############
-%  Ca1,Ca2 is the object get from cafrompdb.m 
-%return newCA1 and newCA2 is the object get from cafrompdb. But only contain the same Ca atom.
+%######## need dict.mat,PA.m,getAtomByAtomName,getbackbone#############
+% input:
+%  Ca1
+%  Ca2 is the object get from readPDB
+% return:
+%   newCA1
+%   newCA2 is the object get from readPDB
+%######## need dict.mat,PA.m,getAtomByAtomName,getbackbone#############
     load('dict.mat');
-    alltmpID1 = [ca1.tmpResID];
-    alltmpID2 = [ca2.tmpResID];
+    alltmpID1 = [ca1.internalResno];
+    alltmpID2 = [ca2.internalResno];
     tempca1=getAtomByAtomName(ca1,'CA');
     tempca2=getAtomByAtomName(ca2,'CA');
 try
@@ -14,7 +19,7 @@ try
     for i=1:length(tempca1)
         if dict.containsKey(tempca1(i).resname)
             resName1(indexOfres,1) = dict.get(tempca1(i).resname);
-            resno1(indexOfres,1) = tempca1(i).tmpResID;
+            resno1(indexOfres,1) = tempca1(i).internalResno;
             indexOfres=indexOfres+1;
         end
     end
@@ -23,7 +28,7 @@ try
     for i=1:length(tempca2)
         if dict.containsKey(tempca2(i).resname)
             resName2(indexOfres,1)=dict.get(tempca2(i).resname);
-            resno2(indexOfres,1) = tempca1(i).tmpResID;
+            resno2(indexOfres,1) = tempca2(i).internalResno;
             indexOfres=indexOfres+1;
         end
     end
@@ -33,11 +38,47 @@ try
     %get the fist non-zero column index
     newCA1=ca1(ismember(alltmpID1,resno1(Index_Seq1_Seq2(1,col(1):end))));
     newCA2=ca2(ismember(alltmpID2,resno2(Index_Seq1_Seq2(2,col(1):end))));
-    if length(newCA1)~=length(newCA2)
-        baseException = MException('core:oneChainExtractSameCA','duplicate resid');
-        throw(baseException);
+    %%%%%% new for test
+    atomPerRes1 = getAtomNumPerRes(newCA1);
+    atomPerRes2 = getAtomNumPerRes(newCA2);
+    check = atomPerRes1 ~= atomPerRes2;
+    if sum(check) ~=0
+        newInternalResno1 = [newCA1.internalResno];
+        uniqueInternalResno1 = unique(newInternalResno1);
+        newInternalResno2 = [newCA2.internalResno];
+        uniqueInternalResno2 = unique(newInternalResno2);
+        diffAtomIndex1 = ismember(newInternalResno1,uniqueInternalResno1(check));
+        diffAtomIndex2 = ismember(newInternalResno2,uniqueInternalResno2(check));
+        % extract backbone
+        diffatomName1 = {newCA1(diffAtomIndex1).atmname};
+        diffatomName2 = {newCA2(diffAtomIndex2).atmname};
+        diffBackboneIndex1 = ismember(diffatomName1,{'C' 'O' 'N' 'CA'});
+        diffBackboneIndex2 = ismember(diffatomName2,{'C' 'O' 'N' 'CA'});
+        % check is the remain atom same.
+        if sum(diffBackboneIndex1) == sum(diffBackboneIndex2)
+            % remove the different atom except backbone atome
+            diffBackboneIndex1(:) = ~diffBackboneIndex1;
+            diffAtomIndex1(diffAtomIndex1) = diffBackboneIndex1;
+            newCA1(diffAtomIndex1) = [];
+            % remove the different atom except backbone atome
+            diffBackboneIndex2(:) = ~diffBackboneIndex2;
+            diffAtomIndex2(diffAtomIndex2) = diffBackboneIndex2;
+            newCA2(diffAtomIndex2) = [];
+        else
+            diffBackboneIndex1 = strcmp(diffatomName1,'CA');
+            diffBackboneIndex2 = strcmp(diffatomName2,'CA');
+            % remove the different atom except backbone atome
+            diffBackboneIndex1(:) = ~diffBackboneIndex1;
+            diffAtomIndex1(diffAtomIndex1) = diffBackboneIndex1;
+            newCA1(diffAtomIndex1) = [];
+            % remove the different atom except backbone atome
+            diffBackboneIndex2(:) = ~diffBackboneIndex2;
+            diffAtomIndex2(diffAtomIndex2) = diffBackboneIndex2;
+            newCA2(diffAtomIndex2) = [];
+        end
     end
 catch e
+    rethrow(e);
     baseException = MException('core:oneChainExtractSameCA',['can not align chain ' ca1(1).subunit ' of pdbStructure1 to chain ' ca2(1).subunit ' of pdbStructure2']);
     baseException = addCause(baseException,e);
     throw(baseException);
