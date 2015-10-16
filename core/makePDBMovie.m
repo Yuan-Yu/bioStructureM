5a768eeab751e3185e7d1cb5d947e931385a5347
Yuan Yu <tearsgundam@gmail.com>
2015-10-16 23:42:51 +0800

    makePDBMovie > add description and fix the function name

--- core/makePDBMovie.m
+++ core/makePDBMovie.m
@@ -1,12 +1,19 @@
-function [movie] = MakePDBMovie(PDBstructure,motion,step,scale)
-%%%%%%%%%%% need
-%
-%
+function [movie] = makePDBMovie(PDBstructure,motion,step,scale)
+%%%%%%%%%%%%%%%%%%% need vector2coord,getCoord %%%%%%%%%%%%%%%%%%%%%%
+% input:
+%   PDBstructure: the PDB structure array 
+%   motion : an array that represent the movement of the PDB structure (3N or N*3)
+%           where the N is the atom number of PDB structure
+%   step : the number of frame would be 4*step
+%   scale: the maximum movement is scale*motion.
+% return:
+%   movie: a cell that eatch element is a frame (pdb structure)
+%%%%%%%%%%%%%%%%%%% need vector2coord,getCoord %%%%%%%%%%%%%%%%%%%%%%
 [~,colnum]=size(motion);
 if colnum == 1
     motion = vector2coord(motion);
 end
-originCrd = getCoordfromca(PDBstructure);
+originCrd = getCoord(PDBstructure);
 steplength = scale/step;
 stepfactor = [0:step step-1:-1:-step -(step-1):-1];
 movie = cell(1,length(stepfactor));
