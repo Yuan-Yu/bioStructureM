9042bf1d0046ec9367869793383372318bd099eb
Yuan Yu <tearsgundam@gmail.com>
2016-06-04 23:44:12 +0800

    modify the output of ENM

--- ENMAnalysis/GNM.m
+++ ENMAnalysis/GNM.m
@@ -1,23 +1,35 @@
-function [pdb]=GNM(pdb,mode,cutOff)
-%%%%%%%%%%%%% need getContactMatrix.m,getPairwiseDistance.m%%%%%%%%%%%%%%%%%%
+function [pdbStructure,GNMValue]=GNM(pdbStructure,mode,cutOff)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % input:
-%   pdb is the structure that gotten from cafrompdb.
+%   pdbStructure is the structure that gotten from cafrompdb.
 %   mode is a number the meaning is how many mode do you want.
 %   cutOff: if the distance of two atom is less than cutOff,the two atom
 %       is contact to each other.
 %
 % return:
-%   pdb is the structure that contain GNM attribute.(ex pdb(indexOfAtom).GNM or pdb(indexOfAtom).GNMValue)
+%   pdbStructure is the structure that contain GNM attribute.(ex pdbStructure(indexOfAtom).GNM or pdbStructure(indexOfAtom).GNMValue)
 %   the format is like
-%       pdb(indexOfAtom).GNM(modth)=the eigenvector of the n GNM mode of the atom.
-%       pdb(indexOfAtom).GNMValue(modth)=the eigvalue of the n GNM mode.
+%       pdbStructure(indexOfAtom).GNM(modth)=the eigenvector of the n GNM mode of the atom.
+%       pdbStructure(indexOfAtom).GNMValue(modth)=the eigvalue of the n GNM mode.
 %
-%%%%%%%%%%%%% need getContactMatrix.m,getPairwiseDistance.m%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% set parameter%%%
+if ~exist('mode','var')
+    mode = length(pdbStructure) - 1;
+end
 if ~exist('cutOff','var')
     cutOff=7.3;
 end
+%%%%%%%%%%%%%%%%%%
 
-contactMatrix=getContactMatrix(pdb,cutOff);
+% check parameter%%%
+if mode+1>length(pdbStructure)
+    error('Useful_func:argumentWrong','mode exceeds number of residues');
+end
+%%%%%%%%%%%%%%%%%%%%
+
+contactMatrix=getContactMatrix(pdbStructure,cutOff);
 [U,S]=eig(contactMatrix);
 S=diag(S);
 
@@ -28,11 +40,10 @@ if S(2)<10^(-9)
 	error('Useful_func:eigError','warning: There are more than one zero eigenvalue.')
 end
 lastMode=1+mode;
-for i=1:length(pdb)
+for i=1:length(pdbStructure)
     for j=2:lastMode
-        pdb(i).GNM(j-1,1)=U(i,j);
-        pdb(i).GNMValue(j-1)=S(j);
+        pdbStructure(i).GNM(j-1,1)=U(i,j);
     end
 end
-
+GNMValue = S(2:lastMode);
 end
