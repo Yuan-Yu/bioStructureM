06b81077441108f209816725922ddf1a9b65b186
Yuan Yu <tearsgundam@gmail.com>
2016-07-03 21:39:03 +0800

    fix a serious bug in decomposeRotation. PLS review the your code that was related to this function.

--- core/decomposeRotation.m
+++ core/decomposeRotation.m
@@ -1,5 +1,8 @@
 function [x,y,z] = decomposeRotation(rotationalMatrix)
 %%%%%%%%%%%%%%%%%%%%%%%%%%
+% decompose a rotation matrix. The ration matrix is matrix which should be 
+%  operated at right. ex. B = A*R ,where the A and B is row vector and B is 
+%  rotated A by operated R matrix.
 % input:
 %   rotationalMatrix: a rotational Matrix is formed by argument
 % return:
@@ -7,6 +10,7 @@ function [x,y,z] = decomposeRotation(rotationalMatrix)
 %   y: rotational angle by y axis in rad 
 %   z: rotational angle by z axis in rad 
 %%%%%%%%%%%%%%%%%%%%%
+    rotationalMatrix = rotationalMatrix';
 	x = atan2(rotationalMatrix(3,2), rotationalMatrix(3,3));
 	y = atan2(-rotationalMatrix(3,1), sqrt(rotationalMatrix(3,2)*rotationalMatrix(3,2) + rotationalMatrix(3,3)*rotationalMatrix(3,3)));
 	z = atan2(rotationalMatrix(2,1), rotationalMatrix(1,1));
