826b5928d73dc169a174a7940c3d137a9185b60d
YuanYu <tearsgundam@gmail.com>
2017-04-21 00:37:40 +0800

    make basicSelector_sequence more smart

--- atomselector/basicSelector_sequence.m
+++ atomselector/basicSelector_sequence.m
@@ -8,7 +8,13 @@ function [logicIndexArray]=basicSelector_sequence(PDBStructure,selectionCell)
 %%%%%%%%%%%%%% need %%%%%%%%%%%%%
 
 %parse selectionString
-targetSeq = selectionCell{1};
+if length(selectionCell) ==2
+    targetSeq = selectionCell{1};
+    tolerance = str2double(selectionCell{2});
+else
+    targetSeq = selectionCell{1};
+    tolerance = 0.8;
+end
 % check which atom will be selection
 allNames = {PDBStructure.atmname};
 allResnames = {PDBStructure.resname};
@@ -17,9 +23,14 @@ allChainIDs = [PDBStructure.subunit];
 
 proteinIndexArray=strcmp(allNames,'CA');
 proteinIndexArray(strcmp(allResnames,'CA')) = 0;
+
+
 matchedInternalResnos = [];
 if sum(proteinIndexArray) ~= 0
     load dict.mat
+    load aa2order.mat
+    load blosum62.mat
+    prefectTreshod = sum(arrayfun(@(x) blosum(aa2order(x),aa2order(x)),targetSeq));
     proteinResnames = allResnames(proteinIndexArray);
     proteinChainIDs = allChainIDs(proteinIndexArray);
     proteinInternalResnos = allInternalResnos(proteinIndexArray);
@@ -33,7 +44,7 @@ if sum(proteinIndexArray) ~= 0
         catch e
             throw(MException('atomSelector:SequenceTransformError','unknown amino acid code'));
         end
-        alignResult = alignSeqRepeatProt(sequence,targetSeq);
+        alignResult = alignSeqRepeatProt(sequence,targetSeq,-4,-prefectTreshod*tolerance);
         matchPart = alignResult(2,alignResult(1,:)~='-') ~= '.';
         matchedInternalResnos = [matchedInternalResnos currentInternalResnos(matchPart)];
     end
